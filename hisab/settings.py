f644c1f37990b8df9b4429c63aefb22640822e3f
Roshan-Prajapati <roshanprajapati865@gmail.com>
2025-09-11 12:24:45 +0530

    update

--- hisab/settings.py
+++ hisab/settings.py
@@ -140,5 +140,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ('prajapatiroshan865@gmail.com')
-EMAIL_HOST_PASSWORD = ('')
+EMAIL_HOST_PASSWORD = ('zvuv mlcv srdw zdmn')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
